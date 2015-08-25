4a06366b81d6cde3838ad23ab54f9385414cb60d
gul <gul@pwny.themissinglink.com.au>
2015-08-25 17:28:34 +1000

    Major improvements ! Not really, it's just debug_utils...

--- debug_utils.rb
+++ debug_utils.rb
@@ -6,37 +6,41 @@ $color=false unless RUBY_PLATFORM.match("w32").nil?
 $debug=false
 
 def colorize(color, text)
-  return "#{color}#{text}\033[0m" if $color == true
+  return "#{color}#{text}\x1b[0m" if $color == true
   text
 end
 
-def black(text);           colorize("\033[30m", text); end
-def red(text);             colorize("\033[31m", text); end
-def green(text);           colorize("\033[32m", text); end
-def yellow(text);          colorize("\033[33m", text); end
-def blue(text);            colorize("\033[34m", text); end
-def magenta(text);         colorize("\033[35m", text); end
-def cyan(text);            colorize("\033[36m", text); end
-def white(text);           colorize("\033[37m", text); end
+def black(text);           colorize("\x1b[30;1m", text); end
+def red(text);             colorize("\x1b[31;1m", text); end
+def green(text);           colorize("\x1b[32;1m", text); end
+def yellow(text);          colorize("\x1b[33;1m", text); end
+def blue(text);            colorize("\x1b[34;1m", text); end
+def magenta(text);         colorize("\x1b[35;1m", text); end
+def cyan(text);            colorize("\x1b[36;1m", text); end
+def white(text);           colorize("\x1b[37;1m", text); end
 
 def print_debug(msg)
-  print_msg("[#{cyan('?')}]", msg) if $debug == true
+  print_msg("[#{blue('DEBUG')}]", msg) if $debug == true
 end
 
 def print_info(msg)
-  print_msg("[#{blue('-')}]", msg)
+  print_msg("[#{cyan('INFO')}]", msg)
 end
 
 def print_ok(msg)
-  print_msg("[#{green('+')}]", msg)
+  print_msg("[#{green('SUCCESS')}]", msg)
 end
 
 def print_nok(msg)
-  print_msg("[#{red('-')}]", msg)
+  print_msg("[#{magenta('FAIL')}]", msg)
+end
+
+def print_warn(msg)
+  print_msg("[#{yellow('WARN')}]", msg)
 end
 
 def print_error(msg)
-  print_msg("[#{red('!')}]", msg)
+  print_msg("[#{red('ERROR')}]", msg)
 end
 
 def print_msg(tok, msg)
